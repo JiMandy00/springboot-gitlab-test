b927324f851e4d69d1b44113871bc9bdd191cb2c
DESKTOP-BAFHRBB\c <wkdghk24@gmail.com>
2022-12-08 14:36:49 +0900

    2022 12 08 - first commit

---
+++ src/main/java/com/gitlab/test/SpringbootGitlabTestApplication.java
@@ -0,0 +1,13 @@
+package com.gitlab.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitlabTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitlabTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gitlab/test/SpringbootGitlabTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitlab.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitlabTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
